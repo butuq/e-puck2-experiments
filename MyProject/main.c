b8b8b4ec408c5498f5ddfe8a086bab8da74f8355
butuq <you@example.com>
2021-05-10 18:01:01 +0100

    Working example after lab 2

--- MyProject/main.c
+++ MyProject/main.c
@@ -21,9 +21,10 @@ MUTEX_DECL(bus_lock);
 CONDVAR_DECL(bus_condvar);
 
 int sensor_data[8], front_sensor;
-int left_v=0, right_v=0;
-float lmt = 0.05;
-
+int last[8];
+float left_v=0, right_v=0,lastv,lastr;
+float lmt = 150,lmt2 = 200,lmt3=25,sdlmt=200,sdlmt2=250;
+float frntlmt = 200;
 
 //Modes of operation
 void mode_1(void);
@@ -40,18 +41,25 @@ void left_side(void);
 void search2(void);
 
 //Globals mode1 
-int sgn=1, random_dir=1, searchl=300, searchr=500,extra=0,rot_spd=300;
+int sgn=1, random_dir=1, searchl=500, searchr=600,extra=0,rot_spd=300,cont=0;
 float k = 75;
+int mem =0;
 //Globals mode2
-int rot_spd2=200,ref_midpoint = 0,kp=40,ki=10,searchl2=200,searchr2=100;
-
+int rot_spd2=350,ref_midpoint = 0,searchl2=350,searchr2=200;
+float kp=0.28,ki=0;
 float acc_errorlef,acc_errorrig,errorlef,errorrig;
 float error1=0,error2=0,acc_errorlef=0,acc_errorrig=0;
 
 
+//////
+// separate more from sensor[5] when rotating so the trajectoryu separates more from the wall.
+// Should be less sensible of case when entering to paralel walls. It shouldnt start rotating.
+//Work in something so that it completes the U trajectory not just one sector of the map.
+// Maybe try bigger kp
+
 int main(void)
 {
-  ref_midpoint =lmt/2;
+  ref_midpoint =175; //could be 175
   //Initialize default
   messagebus_init(&bus, &bus_lock, &bus_condvar);
   halInit();
@@ -82,13 +90,19 @@ int main(void)
       {
         sensor_data[i] = get_calibrated_prox(i);
       }
-      
-      char str[100];
-      int value=10;
-      int str_length=sprintf(str, "Printing number %d!\n",value);
-      set_led(LED1,2);
-      e_send_uart1_char(str, str_length); //Send data using bluetooth
-      chThdSleepMilliseconds(1000);
+/*
+      //Filter sensor data
+      for(int i=0; i<8;i++)
+      {
+        sensor_data[i] = (sensor_data[i] + last[i])/2;
+      }
+
+      //Update last value
+      for(int i=0; i<8;i++)
+      {
+        last[i] = sensor_data[i];
+      }*/
+
       
       //Mode selector
       switch(selc)
@@ -106,8 +120,25 @@ int main(void)
       default:
         left_v = 0;
         right_v = 0;
+
+        char str[100];
+        int str_length=sprintf(str, "Printing number %d\n",sensor_data[1]);
+        set_led(LED1,2);
+        e_send_uart1_char(str, str_length); //Send data using bluetooth
+        chThdSleepMilliseconds(1000);
         break;
       }
+
+      char str[100];
+	  int str_length=sprintf(str, "Printing number %d\n",sensor_data[1]);
+	  //set_led(LED1,2);
+	  e_send_uart1_char(str, str_length); //Send data using bluetooth
+
+	 /* left_v = (left_v + lastv)/2;
+	  right_v = (right_v + lastr)/2;
+
+	  lastv = left_v;
+	  lastr = right_v;*/
 
       left_motor_set_speed(left_v);
       right_motor_set_speed(right_v);
@@ -143,13 +174,17 @@ void __stack_chk_fail(void)
 //Mode 1: Explore
 void mode_1()
 {
-  if (sensor_data[0]<lmt || sensor_data[1]<lmt || sensor_data[2]<lmt || sensor_data[3]<lmt)
+	//Wall detected, move away
+  if ((((sensor_data[0]>lmt || sensor_data[5]>sdlmt) || sensor_data[6]>sdlmt || sensor_data[7]>lmt) && (mem == 1)) || ((sensor_data[0]>lmt2 || sensor_data[5]>sdlmt2 || sensor_data[6]>sdlmt2 || sensor_data[7]>lmt2) && (mem == 0)) )
   {
   	rotate();
-  	extra = 0;
+  	mem = 1;
   	return;
   }
+
+  //Search for wall and explore.
   	search();
+  	mem=0;
 }
 
 
@@ -160,22 +195,22 @@ void mode_2()
   
   search2();
   //Found something by right side
-  if (sensor_data[4]<lmt || sensor_data[5]<lmt)
+  if (sensor_data[1]>lmt3 || sensor_data[2]>lmt3)
   {
     right_side();
   }      
 	//Found something by left side
-	if (sensor_data[1]<lmt || sensor_data[0]<lmt)
+	if (sensor_data[5]>lmt3 || sensor_data[6]>lmt3)
 	{
 	  left_side();
 	}
 	//Found something in the back
-	if (sensor_data[6]<lmt || sensor_data[7]<lmt)
+	if (sensor_data[4]>lmt3 || sensor_data[3]>lmt3)
 	{
 	  right_side();
 	}
 	//Found something in the front
-	if (sensor_data[2]<lmt || sensor_data[3]<lmt)
+	if (sensor_data[0]>frntlmt || sensor_data[7]>frntlmt)
 	{
 	  lock_front();
 	}
@@ -185,7 +220,7 @@ void mode_2()
 //Functions mode 1
 void search()    //Move around to explore
 {
-  extra = extra + 0.01;
+  //extra = extra + 0.01;
   left_v = searchl;
   right_v = searchr;
 }
@@ -202,13 +237,24 @@ void lock_front()
 
 	//PID controller
 	//Error
-	errorlef = sensor_data[2] - ref_midpoint;
-	errorrig = sensor_data[3] - ref_midpoint;
+	errorlef = -sensor_data[7] + ref_midpoint;
+	errorrig = -sensor_data[0] + ref_midpoint;
 	acc_errorlef = acc_errorlef + errorlef;
 	acc_errorrig = acc_errorrig + errorrig;
 
+
+
 	left_v = kp*errorlef + ki*acc_errorlef;
 	right_v = kp*errorrig + ki*acc_errorrig;
+
+	if(left_v < 150 && left_v > -150)
+	{
+		left_v=0;
+	}
+	if(right_v < 150 && right_v > -150)
+	{
+		right_v=0;
+	}
 
 }
 void right_side()
